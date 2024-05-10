2bd0a2eed566044646b71fedf61eb24b12c3b11b
hah12gamer <hah12gamer@gmail.com>
2024-05-10 09:42:17 -0400

    Ticket
    This class allows you to create a ticket in order to get something done

--- Rentron/app/src/main/java/com/example/rentron/Ticket.java
+++ Rentron/app/src/main/java/com/example/rentron/Ticket.java
@@ -1,4 +1,47 @@
 package com.example.rentron;
 
 public class Ticket {
+    private String type;
+    private String message;
+    private int urgency;
+
+    public Ticket(String type,String message,int urgency){
+        setType(type);
+        setMessage(message);
+        setUrgency(urgency);
+    }
+    private void setType(String type){
+        boolean test=false;
+        for (String element:new String[] {"Maintenance","Security","Damage","Infestation"}){
+            if (element.equals(type)){
+                test=true;
+                break;
+            }
+        }
+        if(test){
+            this.type=type;
+        }
+        else{
+            throw new IllegalArgumentException(type+" is not a type please ");
+        }
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public void setUrgency(int urgency){
+        boolean test=false;
+        for(int element:new int[]{1,2,3,4,5}){
+            if(element==urgency){
+                test=true;
+                break;
+            }
+        }
+        if(test){
+            this.urgency=urgency;
+        }
+        else{
+            throw new IllegalArgumentException("Urgency must be between 1 and 5 where 1 is low urgency and 5 is high urgency");
+        }
+    }
 }
