6e38f2750c2a4ccfa5fe0bb88f7b2e94b623642b
hah12gamer <hah12gamer@gmail.com>
2024-05-10 08:52:45 -0400

    Property
    In property the defined methods to create are temporary. A setType method is required to make sure the type is within the types that we have. On the UI standpoint it can be represented as a popup menu or some other type of menu to make sure they don't put something that doesn't exist

--- Rentron/app/src/main/java/com/example/rentron/Property.java
+++ Rentron/app/src/main/java/com/example/rentron/Property.java
@@ -1,4 +1,42 @@
 package com.example.rentron;
 
 public class Property {
+    private Address address;
+    private String type;
+    private double rooms;
+    private double bathrooms;
+    private double floors;
+    private int area;
+    private boolean laundry;
+    private int parking;
+    private double rent;
+    private boolean hydro;
+    private boolean heating;
+    private boolean water;
+    public Property(Address address,String type,
+                    double rooms, double bathrooms, double floors,double rent,int parking,
+                    int area,boolean laundry,boolean hydro,boolean heating,boolean water){
+        setUtilties(hydro,heating,water);
+        setMain(address,type,rooms,bathrooms,floors,area,rent);
+        setAmenities(parking,laundry);
+    }
+    public void setUtilties(boolean hydro,boolean heating,boolean water){
+        this.hydro=hydro;
+        this.heating=heating;
+        this.water=water;
+    }
+    public void setAmenities(int parking,boolean laundry){
+        this.laundry=laundry;
+        this.parking=parking;
+    }
+    public void setMain(Address address,String type,double rooms,double bathrooms,double floors,int area,double rent){
+        this.address=address;
+        this.type=type;
+        this.rooms=rooms;
+        this.bathrooms=bathrooms;
+        this.floors=floors;
+        this.area=area;
+        this.rent=rent;
+    }
+
 }
