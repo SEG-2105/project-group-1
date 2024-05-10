926ae8445c5edfbacedb4c8208a75bc62d357fee
hah12gamer <hah12gamer@gmail.com>
2024-05-10 07:58:19 -0400

    User class
    In this User class which will be a super class for all the users of this platform

--- Rentron/app/src/main/java/com/example/rentron/User.java
+++ Rentron/app/src/main/java/com/example/rentron/User.java
@@ -1,4 +1,23 @@
 package com.example.rentron;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private int birthYear;
+    private String password;
+
+    public User(String firstName,String lastName,int birthYear,String emailAddress,String password){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.birthYear=birthYear;
+        this.emailAddress=emailAddress;
+        this.password=password;
+    }
+    public User(String firstName,String lastName,String emailAddress, String password){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.emailAddress=emailAddress;
+        this.password=password;
+    }
 }
