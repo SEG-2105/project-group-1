82e4ad8a69d3b789d75406751e2e54727a2f14ef
hah12gamer <hah12gamer@gmail.com>
2024-05-28 11:29:03 -0400

    Deleting 2.0

--- Rentron20/app/src/main/java/com/example/rentron20/Client.java
+++
@@ -1,7 +0,0 @@
-package com.example.rentron20;
-
-public class Client extends User{
-    public Client(String firstName,String lastName,int birthYear,String emailAddress,String password){
-        super(firstName,lastName,birthYear,emailAddress,password);
-    }
-}
